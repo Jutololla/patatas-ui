57fd6b6c4682121ffa54ba4ae995f9555ee9b1c7
Jutololla <julianangulop@gmail.com>
2022-12-29 02:42:04 -0500

    interceptor for http requests implemented

--- src/app/services/subscribers/subscribers.service.ts
+++ src/app/services/subscribers/subscribers.service.ts
@@ -3,29 +3,16 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { take } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { TokenStorageService } from '../tokenStorage/token-storage.service';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class SubscribersService {
-	constructor(private http: HttpClient, private router: Router) {}
-
-	authenticationToken: string = '';
-	logIn(username: string, password: string) {
-		const url: string = `${environment.subscribersBackEnd}account/login`;
-		const body = { UserName: username, Password: password };
-		this.http.post(url, body).pipe(take(1)).subscribe((data: any) => {
-			if (data.Status === 1) {
-				this.authenticationToken = data.Token;
-				this.router.navigate([ '/dashboard' ]);
-			}
-		}, (error) => null);
-	}
+	constructor(private http: HttpClient, private router: Router,private token: TokenStorageService) {}	
 	getListOfSubscribers(criteria: string, page: number, count: number, sortOrder: string, sortType: number) {
 		const url: string = `${environment.subscribersBackEnd}subscribers/`;
 		const params = { criteria, page, count, sortOrder, sortType };
-		let  headers = new HttpHeaders();
-		headers = headers.append('Authorization', 'Bearer ' + this.authenticationToken);
-		return this.http.get(url, { headers, params });
+		return this.http.get(url, { params });
 	}
 }
